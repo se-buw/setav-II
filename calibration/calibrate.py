7e5f05f2f63b6c83679ffe0c92ced1afc6f60155
Gouthaman Naganathan <gouthamanstark@gmail.com>
2022-12-29 00:00:55 +0100

    Adding camera calibration script and images

---
+++ calibration/calibrate.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*10,3), np.float32)
+objp[:,:2] = np.mgrid[0:10,0:7].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('*.jpeg')
+
+
+
+for fname in images:
+   
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (10,7), None)
+    
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (10,7), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(300)
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+print(mtx)
+print(dist)
+cv.destroyAllWindows()
