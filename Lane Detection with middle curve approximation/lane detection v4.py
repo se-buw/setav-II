d9bbdf104aa1db13a3f462067053c7fbc588b165
Llao11 <11keitaro@gmail.com>
2023-01-27 17:11:20 +0100

    add lane assist Vehicle class

--- Lane Detection with middle curve approximation/lane detection v4.py
+++ Lane Detection with middle curve approximation/lane detection v4.py
@@ -45,83 +45,83 @@ class lane:
 # drawing the lines as per the edges
 
     def center_line_approximation(self,lines,image):
-    
-    	right_lane_min_y=10000
-    	right_lane_max_y=0
-    	left_lane_points=[[],[]]
-    	right_lane_points=[[],[]]
-    	if lines is not None:
-    		for line in lines:
-	                x1,y1,x2,y2 = line[0]
+
+        right_lane_min_y=10000
+        right_lane_max_y=0
+        left_lane_points=[[],[]]
+        right_lane_points=[[],[]]
+        if lines is not None:
+            for line in lines:
+                    x1,y1,x2,y2 = line[0]
                 
-	                slope=(y2-y1)/(x2-x1)
+                    slope=(y2-y1)/(x2-x1)
                 
-	                if slope>0:
-	                	right_lane_points[0].append(x1)
-	                	right_lane_points[0].append(x2)
-	                	right_lane_points[1].append(y1)
-	                	right_lane_points[1].append(y2)
-	               	
-	                	
-                	else:
-                	
-                		left_lane_points[0].append(x1)
-	                	left_lane_points[0].append(x2)
-	                	left_lane_points[1].append(y1)
-	                	left_lane_points[1].append(y2)
-                		
-               	
-    	
-    	res1=np.polyfit(right_lane_points[1],right_lane_points[0],2)
-    	res2=np.polyfit(left_lane_points[1],left_lane_points[0],2)
-    	
-    	right_lane_max_y=max(right_lane_points[1])
-    	right_lane_min_y=min(right_lane_points[1])
-    	
-    	
-    	
-    	y_values=np.linspace(right_lane_min_y,right_lane_max_y,10,dtype=int)
-    	right_x=(res1[0]*y_values**2+res1[1]*y_values+res1[2]).astype(int)
-    	left_x=(res2[0]*y_values**2+res2[1]*y_values+res2[2]).astype(int)
-    	
-    	for i in range(0,9):
-    		x1=right_x[i].item()    #converting numpy.int64 to python int
-    		y1=y_values[i].item()
-    		x2=right_x[i+1].item()
-    		y2=y_values[i+1].item()
-    	
-    		cv.line(image,(x1,y1),(x2,y2),(0,0,255),2)
-    		
-    		
-    		x1=left_x[i].item()    
-    		y1=y_values[i].item()
-    		x2=left_x[i+1].item()
-    		y2=y_values[i+1].item()
-    	
-    		cv.line(image,(x1,y1),(x2,y2),(0,0,255),2)
-    	
-    	
-    	
-    	
-    	x_values_center=((res1[0]*y_values**2+res1[1]*y_values+res1[2])+(res2[0]*y_values**2+res2[1]*y_values+res2[2]))//2
-    	x_values_center=x_values_center.astype(int)			
-    	
-    	for i in range(0,9):
-    		x1=x_values_center[i].item()    #converting numpy.int64 to python int
-    		y1=y_values[i].item()
-    		x2=x_values_center[i+1].item()
-    		y2=y_values[i+1].item()
-    	
-    		cv.line(image,(x1,y1),(x2,y2),(0,255,0),2)
-    		
-    	center_lane_points=[]
-    	center_lane_points.append(x_values_center,y_values)
-    	print(center_lane_points)
-    	publisher.publish(center_lane_points)
-    	
-    	
-    	  
-    	return image
+                    if slope>0:
+                        right_lane_points[0].append(x1)
+                        right_lane_points[0].append(x2)
+                        right_lane_points[1].append(y1)
+                        right_lane_points[1].append(y2)
+                    
+                        
+                    else:
+                    
+                        left_lane_points[0].append(x1)
+                        left_lane_points[0].append(x2)
+                        left_lane_points[1].append(y1)
+                        left_lane_points[1].append(y2)
+                        
+                
+        
+        res1=np.polyfit(right_lane_points[1],right_lane_points[0],2)
+        res2=np.polyfit(left_lane_points[1],left_lane_points[0],2)
+        
+        right_lane_max_y=max(right_lane_points[1])
+        right_lane_min_y=min(right_lane_points[1])
+        
+        
+        
+        y_values=np.linspace(right_lane_min_y,right_lane_max_y,10,dtype=int)
+        right_x=(res1[0]*y_values**2+res1[1]*y_values+res1[2]).astype(int)
+        left_x=(res2[0]*y_values**2+res2[1]*y_values+res2[2]).astype(int)
+        
+        for i in range(0,9):
+            x1=right_x[i].item()    #converting numpy.int64 to python int
+            y1=y_values[i].item()
+            x2=right_x[i+1].item()
+            y2=y_values[i+1].item()
+        
+            cv.line(image,(x1,y1),(x2,y2),(0,0,255),2)
+            
+            
+            x1=left_x[i].item()    
+            y1=y_values[i].item()
+            x2=left_x[i+1].item()
+            y2=y_values[i+1].item()
+        
+            cv.line(image,(x1,y1),(x2,y2),(0,0,255),2)
+        
+        
+        
+        
+        x_values_center=((res1[0]*y_values**2+res1[1]*y_values+res1[2])+(res2[0]*y_values**2+res2[1]*y_values+res2[2]))//2
+        x_values_center=x_values_center.astype(int)			
+        
+        for i in range(0,9):
+            x1=x_values_center[i].item()    #converting numpy.int64 to python int
+            y1=y_values[i].item()
+            x2=x_values_center[i+1].item()
+            y2=y_values[i+1].item()
+        
+            cv.line(image,(x1,y1),(x2,y2),(0,255,0),2)
+            
+        center_lane_points=[]
+        center_lane_points.append(x_values_center,y_values)
+        print(center_lane_points)
+        publisher.publish(center_lane_points)
+        
+        
+            
+        return image
     	
     	
     	
@@ -169,6 +169,7 @@ def main(args=None):
     
     subscription=subscriber_node.create_subscription(CompressedImage,'/image_raw/compressed',convertImage,10)
     publisher=publisher_node.create_publisher(Int16MultiArray,"lane_coordinates",10)
+    
     rclpy.spin(subscriber_node)
     rclpy.spin(publisher_node)
     subscriber_node.destroy_node()
--- car_lane_assist_node/car_lane_assist_node/lane_assist_pub_sub.py
+++ car_lane_assist_node/car_lane_assist_node/lane_assist_pub_sub.py
@@ -6,6 +6,84 @@ from std_msgs.msg import String
 from std_msgs.msg import Int16MultiArray
 
 
+class Vehicle(object):
+
+    #intialial position of vehicle
+    def __init__(self, x=0.0, y=0.0, angle= 0.0, length=50.0):
+        self.difference_distance = 0.0
+        self.difference_rotation = 0.0
+        self.difference_drift = 0.0
+        self.length = length
+
+        #setting coordinates of vehicle
+    def set(self, x, y, angle):
+
+        self.x = x
+        self.y = y
+        self.angle = angle % (2.0 * np.pi)
+
+    def set_difference(self, difference_rotation, difference_distance):
+        self.difference_distance = difference_distance
+        self.difference_rotation = difference_rotation
+
+    def set_difference_drift(self, drift):
+        self.difference_drift = drift
+
+    #steering = angle of front wheel
+
+    def drive(self, steer, distance, tolerance=0.001, max_allowed_steering =np.pi / 4.0):
+         if steer > max_allowed_steering:
+            steer = max_allowed_steering
+         if steer < -max_allowed_steering:
+            steer = -max_allowed_steering
+         if distance < 0.0:
+            distance = 0.0
+
+         steer = random.gauss(steer, self.difference_rotation)
+         distance = random.gauss(distance, self.difference_distance)
+
+         steer += self.difference_drift
+
+         rotate = np.tan(steer * distance / self.length)
+         if abs(rotate) < tolerance:
+            
+            self.x += distance * np.cos(self.angle)
+            self.y += distance * np.sin(self.angle)
+            self.angle = (self.angle + rotate) % (2.0 * np.pi)
+         else: 
+            radius = distance / rotate
+            cx = self.x - (np.sin(self.angle) * radius)
+            cy = self.y + (np.cos(self.angle) * radius)
+            self.angle = (self.angle + rotate) % (2.0 * np.pi)
+            self.x = cx + (np.sin(self.angle) * radius)
+            self.y = cy - (np.cos(self.angle) * radius)
+
+    
+    def run(self, wp, wd, wi, n=100, speed=2.0):
+        x_trajectory = []
+        y_trajectory = []
+        
+        prev_cte = self.y
+        sum_cte = 0
+        for _ in range(n):
+            sum_cte += self.y
+            dev = self.y - prev_cte
+            prev_cte = self.y
+            steer = -wp * self.y - wd * dev - wi*sum_cte
+            self.drive(steer, speed)
+            x_trajectory.append(self.x)
+            y_trajectory.append(self.y)
+            print(steer)
+        
+        # write a code to derive angles from the steer
+        angles = []
+        return angles
+
+    def __repr__(self):
+        return '[x=%.5f y=%.5f orient=%.5f]' % (self.x, self.y, self.angle)
+
+
+
 # Publisher node creation class
 class lane_assist_publisher(Node):
 
@@ -17,7 +95,7 @@ class lane_assist_publisher(Node):
 
     def publish_msg(self):
         self.msg = String()
-        self.msg.data = "stop"
+        self.msg.data = command
         self.publisher_.publish(self.msg)
 
 
@@ -27,10 +105,20 @@ class lane_assist_subscriber(Node):
 
     def __init__(self):
         super().__init__('lane_assist_sub')
-        self.subscription = self.create_subscription(String,'topic',self.listener_callback,10)
+        self.subscription = self.create_subscription(Int16MultiArray,"lane_coordinates",self.listener_callback,10)
 
     def listener_callback(self, msg):
         self.get_logger().info('I also heard: "%s"' % msg.data)
+    
+        vehicle = Vehicle()
+        x = msg.data[0]
+        y = msg.data[1]
+        vehicle.set(x,y)
+        angles = vehicle.run()
+        global command
+        for angle in angles:
+            command= "turn_to_angle %s" %angle
+
 
 
 def main(args=None):
